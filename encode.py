67d9ddf76dd8d52ef7b0d9d57c3688cb7bf2ab67
Iikka Hauhio <iikka.hauhio@helsinki.fi>
2019-12-24 00:07:18 +0200

    muiden tokenisaatiomallien käyttäminen

--- encode.py
+++ encode.py
@@ -4,16 +4,18 @@ import sys
 import fire
 
 sp = spm.SentencePieceProcessor()
-sp.Load("iso-lc.model")
 
-def main(input_file, output_file):
+def main(input_file, output_file, model="iso-lc.model"):
+	sp.Load(model)
 	ids = []
 	with open(input_file, "r") as f:
 		for i, line in enumerate(f):
 			if i%10000 == 0:
 				print(i, end="\r")
 				sys.stdout.flush()
-			ids += [sp.EncodeAsIds(line.lower())]
+			if "-lc" in model:
+				line = line.lower()
+			ids += [sp.EncodeAsIds(line)]
 
 	ids = np.array(ids)
 	np.save(output_file, ids)
